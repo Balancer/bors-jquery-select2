b55c596f8739d497f5ede620282365d5c265ae44
Balancer <balancer@balancer.ru>
2013-04-19 05:32:39 +0400

    Упрощение вызовов select2.

--- classes/jquery/select2.php
+++ classes/jquery/select2.php
@@ -4,7 +4,7 @@
 
 class jquery_select2
 {
-	function appear($el, $attrs)
+	static function appear($el, $attrs)
 	{
 		$base = config('jquery.select2.base');
 		bors_use("$base/select2.css");
@@ -14,5 +14,26 @@ class jquery_select2
 		$attrs = blib_json::encode_jsfunc($attrs);
 //		var_dump($attrs);
 		jquery::on_ready("$({$el}).select2($attrs)\n");
+	}
+
+	static function appear_ajax($el, $class_name, $params = array())
+	{
+		self::appear($el, array(
+			'ajax' => array(
+				'minimumInputLength' => 3,
+//				'placeholder' => "Search for a movie",
+				'url' => '/_bors/data/lists/',
+				'data' => "function (text, page) { return {
+						class: '$class_name',
+						q: text,
+						s: 10,
+						tpl: '{\"id\":\"id\",\"text\":\"".defval($params, 'title_field', 'title')."\"}',
+						order: \"".defval($params, 'order', 'title')."\",
+						search: \"title\",
+						results: \"results\"
+					} }",
+				'results' => 'function (data, page) { return data }',
+			),
+		));
 	}
 }
