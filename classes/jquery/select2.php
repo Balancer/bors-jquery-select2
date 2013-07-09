d99590c612c5c270127db085313470245177b80e
Balancer <balancer@balancer.ru>
2013-07-09 16:20:46 +0400

    ++

--- classes/jquery/select2.php
+++ classes/jquery/select2.php
@@ -11,6 +11,7 @@ class jquery_select2
 		jquery::plugin("$base/select2.min.js");
 		jquery::plugin("$base/select2_locale_ru.js");
 
+//		var_dump($attrs);
 		$attrs = blib_json::encode_jsfunc($attrs);
 //		var_dump($attrs);
 		jquery::on_ready("$({$el}).select2($attrs)\n");
@@ -18,19 +19,25 @@ class jquery_select2
 
 	static function appear_ajax($el, $class_name, $params = array())
 	{
+		$title_field = popval($params, 'title_field', 'title');
+		$order = popval($params, 'order', 'title');
+		$search = popval($params, 'search_fields', 'title');
+		popval($params, 'name');
+		popval($params, 'value');
+		popval($params, 'json');
 //	http://admin2.aviaport.wrk.ru/newses/257920/form2/
 		$params = array_merge($params, array(
 			'ajax' => array(
-				'minimumInputLength' => 3,
-				'placeholder' => "Search for a movie",
+				'minimumInputLength' => 2,
+//				'placeholder' => "Search for a movie",
 				'url' => '/_bors/data/lists/',
 				'data' => "function (text, page) { return {
 						class: '$class_name',
 						q: text,
 						s: 10,
-						tpl: '{\"id\":\"id\",\"text\":\"".defval($params, 'title_field', 'title')."\"}',
-						order: \"".defval($params, 'order', 'title')."\",
-						search: \"".defval($params, 'search_fields', 'title')."\",
+						tpl: '{\"id\":\"id\",\"text\":\"".$title_field."\"}',
+						order: \"".$order."\",
+						search: \"".$search."\",
 						results: \"results\"
 					} }",
 				'results' => 'function (data, page) { return data }',
